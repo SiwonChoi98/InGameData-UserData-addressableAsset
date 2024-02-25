aec4981bb3c7f907d5fdf74bfaa5dd025033ca06
CHOI <qpqp615@gmail.com>
2024-02-25 21:36:26 +0900

    [Feat_Asset] 추가 유닛 생성 에셋 추가

---
+++ Assets/Projects/Scripts/Manager/AddressableManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class AddressableManager : MonoBehaviour
+{
+    [SerializeField] private AssetReferenceGameObject _test1Obj;
+
+    //오브젝트를 불러오면 나중에 해제를 했을 때 담아줄 리스트
+    private List<GameObject> _GsList = new List<GameObject>();
+    private void Start()
+    {
+        StartCoroutine(InitAddressable());
+        SpawnObj();
+    }
+
+    private void Update()
+    {
+        ReleaseObj();
+    }
+
+    //어드레서블 초기화 (혹시나 안될 수 있어서 막기위해 사용)
+    private IEnumerator InitAddressable()
+    {
+        var init = Addressables.InitializeAsync();
+        yield return init;
+    }
+
+    private void SpawnObj()
+    {
+        _test1Obj.InstantiateAsync().Completed += (obj) =>
+        {
+            _GsList.Add(obj.Result);
+        };
+    }
+
+    private void ReleaseObj()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (_GsList.Count == 0)
+                return;
+
+            int index = _GsList.Count - 1;
+            
+            //InstantiateAsync으로 생성된 오브젝트의 메모리를 해제하고 제거함
+            Addressables.ReleaseInstance(_GsList[index]);
+            _GsList.RemoveAt(index);
+        }
+    }
+}
